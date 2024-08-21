0dca6c221fe2b57eb2adab4e6c143f5708f73e9b
Arsalan Ahmad (2823) <arsalan.ahamad@nxb.com.pk>
2024-08-21 16:28:52 +0500

    Made Generes Data dynamic

---
+++ src/data/GenericsData.js
@@ -0,0 +1,15 @@
+const genres = [
+    { name: 'Romance', imgSrc: '../../public/images/heartFull.svg' },
+    { name: 'Mystery Thriller & Suspense', imgSrc: '../../public/images/light.svg' },
+    { name: 'Money & Finance', imgSrc: '../../public/images/money.svg' },
+    { name: 'Science Fiction & Fantasy', imgSrc: '../../public/images/star.svg' },
+    { name: 'Literature & Fiction', imgSrc: '../../public/images/book.svg' },
+    { name: 'Biographies & Memoirs', imgSrc: '../../public/images/inkPot.svg' },
+    { name: 'Self-Improvement', imgSrc: '../../public/images/heart.svg' },
+    { name: 'Erotica', imgSrc: '../../public/images/fire.svg' },
+    { name: 'Politics & Social Sciences', imgSrc: '../../public/images/fire.svg' },
+    { name: 'Relationships', imgSrc: '../../public/images/innerHeart.svg' },
+    { name: 'Startups, Business & Careers', imgSrc: '../../public/images/case.svg' },
+    { name: 'Health & Wellness', imgSrc: '../../public/images/heartCheck.svg' },
+];
+export default genres;
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,3 +1,5 @@
+import genres from "../data/GenericsData.js";
+
 const HomeScreen = () => {
     return (
         <div className="absolute h-screen bg-cover w-screen bg-center mt-40"
@@ -16,7 +18,7 @@ const HomeScreen = () => {
                        className="text-[16px] font-normal text-[#5D5D5D] pb-2">New to UPLIT? Get a free title on us.</p>
                     <button type="button"
                             style={{fontFamily: 'Outfit, sans-serif'}}
-                            className="focus:outline-none w-[385px] h-[52px] text-[#232323]-400 bg-[#FFAF19] hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900">Try
+                            className="focus:outline-none sm:w-[200px] w-[385px] h-[52px] text-[#232323]-400 bg-[#FFAF19] hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900">Try
                         for 0.01$
                     </button>
                     <p style={{fontFamily: 'Outfit, sans-serif'}}
@@ -24,18 +26,18 @@ const HomeScreen = () => {
                         anytime.</p>
                 </div>
             </div>
-            <div className="relative z-10 flex justify-center -mt-8 ">
-                <div className="flex flex-row items-center justify-center">
-                    <img className="h-auto max-w-full" src="../../public/images/mayBeSomeDay.png"
+            <div className="relative z-10 flex justify-center -mt-8">
+                <div className="flex flex-col md:flex-row items-center justify-center">
+                    <img className="h-auto max-w-full mb-4 md:mb-0" src="../../public/images/mayBeSomeDay.png"
                          alt="image description"/>
-                    <div className="flex flex-col items-center justify-center pl-4">
+                    <div className="flex flex-col items-center justify-center pl-0 md:pl-4">
                         <img className="h-auto max-w-full pb-4" src="../../public/images/reminders.png"
                              alt="image description"/>
                         <img className="h-auto max-w-full" src="../../public/images/houseMaid.png"
                              alt="image description"/>
                     </div>
                     <div
-                        className="relative inline-block ml-8 mr-4 p-3 rounded-3xl"
+                        className="relative inline-block ml-0 md:ml-8 mr-0 md:mr-4 mt-4 md:mt-0 p-3 rounded-3xl"
                         style={{
                             background: 'linear-gradient(180deg, #875FF6 0%, #07030C 100%)',
                             borderRadius: '1rem',
@@ -51,134 +53,45 @@ const HomeScreen = () => {
                         />
                         <div className="absolute bottom-0 left-0 w-full" style={{backgroundColor: '#07030C',}}></div>
                     </div>
-                    <div className="flex flex-col items-center justify-center pl-4 pr-4">
+                    <div className="flex flex-col items-center justify-center pl-0 md:pl-4 pr-0 md:pr-4 mt-4 md:mt-0">
                         <img className="h-auto max-w-full pb-4" src="../../public/images/mist.png"
                              alt="image description"/>
                         <img className="h-auto max-w-full" src="../../public/images/elegy.png"
                              alt="image description"/>
                     </div>
-                    <img className="h-auto max-w-full" src="../../public/images/ugly.png"
+                    <img className="h-auto max-w-full mt-4 md:mt-0" src="../../public/images/ugly.png"
                          alt="image description"/>
                 </div>
             </div>
+
             <div className="flex flex-col items-center justify-center mt-28 pb-10">
                 <p style={{fontFamily: 'Outfit, sans-serif'}}
                    className="text-[30px] font-bold text-[#454545] pb-20 w-[73%] text-center">Explore what’s trending
                     with millions of listeners, powered by AI recommendations tailored just for you</p>
-                <div className="flex flex-row items-center justify-center">
-                    <img className="h-auto max-w-full pr-4" src="../../public/images/navola.png"
+                <div className="flex flex-col md:flex-row items-center justify-center">
+                    <img className="h-auto max-w-full mb-4 md:mb-0 md:mr-4" src="../../public/images/navola.png"
                          alt="image description"/>
-                    <img className="h-auto max-w-full pr-4 opacity-3" src="../../public/images/unbound.png"
+                    <img className="h-auto w-3/4 md:w-[90%] mb-4 md:mb-0 md:mr-4 opacity-75"
+                         src="../../public/images/unbound.png"
                          alt="image description"/>
-                    <img className="h-auto max-w-full" src="../../public/images/jasmine.png"
+                    <img className="h-auto max-w-full mb-4 md:mb-0" src="../../public/images/jasmine.png"
                          alt="image description"/>
                 </div>
+
             </div>
             <div className="flex flex-col items-center justify-center mt-2 pb-14">
                 <p style={{fontFamily: 'Outfit, sans-serif'}}
                    className="text-[32px] sm:text-[45px] font-semibold text-[#454545] w-2/5 text-center pt-14">Genres</p>
-                <div className="flex flex-wrap justify-center gap-6 mt-4">
-                    <div className="flex flex-col items-center justify-center pt-2">
-                        <div
-                            className="flex items-center h-full w-[250px] md:w-[220px] pl-4 py-7 pr-8 rounded-2xl bg-[#3D3D3D]">
-                            <img className="h-auto max-w-full" src="../../public/images/heartFull.svg"
-                                 alt="image description"/>
-                            <p className="pl-2 font-bold text-white text-[12px]">Romance</p>
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mt-4">
+                    {genres.map((genre, index) => (
+                        <div key={index} className="flex flex-col items-center justify-center pt-2">
+                            <div
+                                className="flex items-center h-full w-[250px] md:w-[220px] pl-4 py-7 pr-8 rounded-2xl bg-[#3D3D3D]">
+                                <img className="h-auto max-w-full" src={genre.imgSrc} alt={`${genre.name} icon`}/>
+                                <p className="pl-2 font-bold text-white text-[12px]">{genre.name}</p>
+                            </div>
                         </div>
-                    </div>
-                    <div className="flex flex-col items-center justify-center pt-2">
-                        <div
-                            className="flex items-center h-full w-[250px] md:w-[220px] pl-4 py-7 pr-8 rounded-2xl bg-[#3D3D3D]">
-                            <img className="h-auto max-w-full" src="../../public/images/light.svg"
-                                 alt="image description"/>
-                            <p className="pl-2 font-bold text-white text-[12px]">Mystery Thriller & Suspense</p>
-                        </div>
-                    </div>
-                    <div className="flex flex-col items-center justify-center pt-2">
-                        <div
-                            className="flex items-center h-full w-[250px] md:w-[220px] pl-4 py-7 pr-8 rounded-2xl bg-[#3D3D3D]">
-                            <img className="h-auto max-w-full" src="../../public/images/money.svg"
-                                 alt="image description"/>
-                            <p className="pl-2 font-bold text-white text-[12px]">Money & Finance</p>
-                        </div>
-                    </div>
-                    <div className="flex flex-col items-center justify-center pt-2">
-                        <div
-                            className="flex items-center h-full w-[250px] md:w-[220px] pl-4 py-7 pr-8 rounded-2xl bg-[#3D3D3D]">
-                            <img className="h-auto max-w-full" src="../../public/images/star.svg"
-                                 alt="image description"/>
-                            <p className="pl-2 font-bold text-white text-[12px]">Science Fiction & Fantasy</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="flex flex-wrap justify-center gap-6 mt-4">
-                    <div className="flex flex-col items-center justify-center pt-2">
-                        <div
-                            className="flex items-center h-full w-[250px] md:w-[220px] pl-4 py-7 pr-8 rounded-2xl bg-[#3D3D3D]">
-                            <img className="h-auto max-w-full" src="../../public/images/book.svg"
-                                 alt="image description"/>
-                            <p className="pl-2 font-bold text-white text-[12px]">Literature & Fiction</p>
-                        </div>
-                    </div>
-                    <div className="flex flex-col items-center justify-center pt-2">
-                        <div
-                            className="flex items-center h-full w-[250px] md:w-[220px] pl-4 py-7 pr-8 rounded-2xl bg-[#3D3D3D]">
-                            <img className="h-auto max-w-full" src="../../public/images/inkPot.svg"
-                                 alt="image description"/>
-                            <p className="pl-2 font-bold text-white text-[12px]">Biographies & Memoirs</p>
-                        </div>
-                    </div>
-                    <div className="flex flex-col items-center justify-center pt-2">
-                        <div
-                            className="flex items-center h-full w-[250px] md:w-[220px] pl-4 py-7 pr-8 rounded-2xl bg-[#3D3D3D]">
-                            <img className="h-auto max-w-full" src="../../public/images/heart.svg"
-                                 alt="image description"/>
-                            <p className="pl-2 font-bold text-white text-[12px]">Self-Improvement</p>
-                        </div>
-                    </div>
-                    <div className="flex flex-col items-center justify-center pt-2">
-                        <div
-                            className="flex items-center h-full w-[250px] md:w-[220px] pl-4 py-7 pr-8 rounded-2xl bg-[#3D3D3D]">
-                            <img className="h-auto max-w-full" src="../../public/images/fire.svg"
-                                 alt="image description"/>
-                            <p className="pl-2 font-bold text-white text-[12px]">Erotica</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="flex flex-wrap justify-center gap-6 mt-4">
-                    <div className="flex flex-col items-center justify-center pt-2">
-                        <div
-                            className="flex items-center h-full w-[250px] md:w-[220px] pl-4 py-7 pr-8 rounded-2xl bg-[#3D3D3D]">
-                            <img className="h-auto max-w-full" src="../../public/images/fire.svg"
-                                 alt="image description"/>
-                            <p className="pl-2 font-bold text-white text-[12px]">Politics & Social Sciences</p>
-                        </div>
-                    </div>
-                    <div className="flex flex-col items-center justify-center pt-2">
-                        <div
-                            className="flex items-center h-full w-[250px] md:w-[220px] pl-4 py-7 pr-8 rounded-2xl bg-[#3D3D3D]">
-                            <img className="h-auto max-w-full" src="../../public/images/innerHeart.svg"
-                                 alt="image description"/>
-                            <p className="pl-2 font-bold text-white text-[12px]">Relationships</p>
-                        </div>
-                    </div>
-                    <div className="flex flex-col items-center justify-center pt-2">
-                        <div
-                            className="flex items-center h-full w-[250px] md:w-[220px] pl-4 py-7 pr-8 rounded-2xl bg-[#3D3D3D]">
-                            <img className="h-auto max-w-full" src="../../public/images/case.svg"
-                                 alt="image description"/>
-                            <p className="pl-2 font-bold text-white text-[12px]">Startups, Business & Careers</p>
-                        </div>
-                    </div>
-                    <div className="flex flex-col items-center justify-center pt-2">
-                        <div
-                            className="flex items-center h-full w-[250px] md:w-[220px] pl-4 py-7 pr-8 rounded-2xl bg-[#3D3D3D]">
-                            <img className="h-auto max-w-full" src="../../public/images/heartCheck.svg"
-                                 alt="image description"/>
-                            <p className="pl-2 font-bold text-white text-[12px]">Health & Wellness</p>
-                        </div>
-                    </div>
+                    ))}
                 </div>
             </div>
         </div>
