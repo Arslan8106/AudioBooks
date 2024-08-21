cedbcbb6af49dba6b5360e750c4504dc2397428f
Arsalan Ahmad (2823) <arsalan.ahamad@nxb.com.pk>
2024-08-21 15:00:10 +0500

    Complete HomePage

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,20 +1,29 @@
 const HomeScreen = () => {
     return (
-        <div className="h-screen w-screen bg-cover bg-center mt-24"
+        <div className="absolute h-screen bg-cover w-screen bg-center mt-40"
              style={{backgroundImage: "url('../../public/images/Vector1.png')"}}>
-            <div className="flex justify-center mb-6">
+            <div className="absolute inset-1 flex justify-center items-center">
+                <div className="w-screen h-4/5 sm:h-4/5 bg-gradient-to-b from-transparent via-[#828CFF] to-transparent opacity-40"></div>
+            </div>
+            <div className="flex justify-center -mt-20 pb-28">
                 <div className="flex flex-col items-center justify-center">
-                    <img className="h-auto max-w-full" src="../../public/images/mainIcon.svg" alt="image description"/>
-                    <h1 className="text-4xl font-bold text-gray-700">Audio Books</h1>
-                    <p className="text-[16px] font-bold text-gray-400">New to UPLIT? Get a free title on us.</p>
+                    <img className="h-auto max-w-full" src="../../public/images/mainIcon.svg"
+                         alt="image description"/>
+                    <h1 style={{fontFamily: 'Outfit, sans-serif'}}
+                        className="text-[40px] font-bold text-[#4B4B4B]">Audio Books</h1>
+                    <p style={{fontFamily: 'Outfit, sans-serif'}}
+                       className="text-[16px] font-normal text-[#5D5D5D] pb-2">New to UPLIT? Get a free title on us.</p>
                     <button type="button"
-                            className="focus:outline-none w-[385px] h-[55px] text-[#232323]-400 bg-[#FFAF19] hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900">Try
+                            style={{fontFamily: 'Outfit, sans-serif'}}
+                            className="focus:outline-none w-[385px] h-[52px] text-[#232323]-400 bg-[#FFAF19] hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900">Try
                         for 0.01$
                     </button>
-                    <p className="text-[14px] font-bold text-gray-400">$14.95 a month after 30 days. Cancel anytime.</p>
+                    <p style={{fontFamily: 'Outfit, sans-serif'}}
+                       className="text-[14px] font-normal text-[#5D5D5D]">$14.95 a month after 30 days. Cancel
+                        anytime.</p>
                 </div>
             </div>
-            <div className="flex justify-center mt-10 ">
+            <div className="relative z-10 flex justify-center -mt-8 ">
                 <div className="flex flex-row items-center justify-center">
                     <img className="h-auto max-w-full" src="../../public/images/mayBeSomeDay.png"
                          alt="image description"/>
@@ -24,7 +33,7 @@ const HomeScreen = () => {
                         <img className="h-auto max-w-full" src="../../public/images/houseMaid.png"
                              alt="image description"/>
                     </div>
-                    <img className="h-auto max-w-full border-8 rounded-3xl border-[#875FF6] ml-8"
+                    <img className="h-auto max-w-full border-8 rounded-3xl border-[#875FF6] ml-8 mr-4"
                          src="../../public/images/AudioBook.png"
                          alt="image description"/>
                     <div className="flex flex-col items-center justify-center pl-4 pr-4">
@@ -37,10 +46,10 @@ const HomeScreen = () => {
                          alt="image description"/>
                 </div>
             </div>
-            <div className="flex flex-col items-center justify-center mt-20 pb-20">
-                <p className="text-[16px] font-bold text-gray-400 pb-10 w-2/5 text-center">Explore what’s trending
-                    with
-                    millions of listeners, powered by AI recommendations tailored just for you</p>
+            <div className="flex flex-col items-center justify-center mt-28 pb-10">
+                <p style={{fontFamily: 'Outfit, sans-serif'}}
+                   className="text-[30px] font-bold text-[#454545] pb-20 w-[73%] text-center">Explore what’s trending
+                    with millions of listeners, powered by AI recommendations tailored just for you</p>
                 <div className="flex flex-row items-center justify-center">
                     <img className="h-auto max-w-full pr-4" src="../../public/images/navola.png"
                          alt="image description"/>
@@ -50,102 +59,114 @@ const HomeScreen = () => {
                          alt="image description"/>
                 </div>
             </div>
-            <div className="flex flex-col items-center justify-center mt-8 pb-14">
-                <p className="text-[45px] font-bold text-black-400 w-2/5 text-center pt-10">Genres</p>
-                <div className="flex flex-row items-stretch justify-center mt-4">
-                    <div className="flex flex-row items-center justify-center pt-2 pr-4">
-                        <div className="flex flex-row items-start pt-7 pb-7 pl-4 pr-24 rounded-2xl bg-[#3D3D3D]">
-                            <img className="h-auto max-w-full" src="../../public/images/cat-icon-1.svg.png"
+            <div className="flex flex-col items-center justify-center mt-2 pb-14">
+                <p style={{fontFamily: 'Outfit, sans-serif'}}
+                   className="text-[32px] sm:text-[45px] font-semibold text-[#454545] w-2/5 text-center pt-14">Genres</p>
+                <div className="flex flex-wrap justify-center gap-6 mt-4">
+                    <div className="flex flex-col items-center justify-center pt-2">
+                        <div
+                            className="flex items-center h-full w-[250px] md:w-[220px] pl-4 py-7 pr-8 rounded-2xl bg-[#3D3D3D]">
+                            <img className="h-auto max-w-full" src="../../public/images/heartFull.svg"
                                  alt="image description"/>
-                            <p className=" pl-2 font-bold text-white">Romance</p>
+                            <p className="pl-2 font-bold text-white text-[12px]">Romance</p>
                         </div>
                     </div>
-                    <div className="flex flex-row items-center justify-center pt-2 pr-4">
-                        <div className="flex flex-row items-start pt-7 pb-7 pl-4 pr-24 rounded-2xl bg-[#3D3D3D]">
-                            <img className="h-auto max-w-full" src="../../public/images/cat-icon-1.svg.png"
+                    <div className="flex flex-col items-center justify-center pt-2">
+                        <div
+                            className="flex items-center h-full w-[250px] md:w-[220px] pl-4 py-7 pr-8 rounded-2xl bg-[#3D3D3D]">
+                            <img className="h-auto max-w-full" src="../../public/images/light.svg"
                                  alt="image description"/>
-                            <p className=" pl-2 font-bold text-white">Romance</p>
+                            <p className="pl-2 font-bold text-white text-[12px]">Mystery Thriller & Suspense</p>
                         </div>
                     </div>
-                    <div className="flex flex-row items-center justify-center pt-2 pr-4">
-                        <div className="flex flex-row items-start pt-7 pb-7 pl-4 pr-24 rounded-2xl bg-[#3D3D3D]">
-                            <img className="h-auto max-w-full" src="../../public/images/cat-icon-1.svg.png"
+                    <div className="flex flex-col items-center justify-center pt-2">
+                        <div
+                            className="flex items-center h-full w-[250px] md:w-[220px] pl-4 py-7 pr-8 rounded-2xl bg-[#3D3D3D]">
+                            <img className="h-auto max-w-full" src="../../public/images/money.svg"
                                  alt="image description"/>
-                            <p className=" pl-2 font-bold text-white">Romance</p>
+                            <p className="pl-2 font-bold text-white text-[12px]">Money & Finance</p>
                         </div>
                     </div>
-                    <div className="flex flex-row items-center justify-center pt-2 pr-4">
-                        <div className="flex flex-row items-start pt-7 pb-7 pl-4 pr-24 rounded-2xl bg-[#3D3D3D]">
-                            <img className="h-auto max-w-full" src="../../public/images/cat-icon-1.svg.png"
+                    <div className="flex flex-col items-center justify-center pt-2">
+                        <div
+                            className="flex items-center h-full w-[250px] md:w-[220px] pl-4 py-7 pr-8 rounded-2xl bg-[#3D3D3D]">
+                            <img className="h-auto max-w-full" src="../../public/images/star.svg"
                                  alt="image description"/>
-                            <p className=" pl-2 font-bold text-white">Romance</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="flex flex-row items-center justify-center">
-                    <div className="flex flex-row items-center justify-center pt-2 pr-4">
-                        <div className="flex flex-row items-start pt-7 pb-7 pl-4 pr-24 rounded-2xl bg-[#3D3D3D]">
-                            <img className="h-auto max-w-full" src="../../public/images/cat-icon-1.svg.png"
-                                 alt="image description"/>
-                            <p className=" pl-2 font-bold text-white">Romance</p>
-                        </div>
-                    </div>
-                    <div className="flex flex-row items-center justify-center pt-2 pr-4">
-                        <div className="flex flex-row items-start pt-7 pb-7 pl-4 pr-24 rounded-2xl bg-[#3D3D3D]">
-                            <img className="h-auto max-w-full" src="../../public/images/cat-icon-1.svg.png"
-                                 alt="image description"/>
-                            <p className=" pl-2 font-bold text-white">Romance</p>
-                        </div>
-                    </div>
-                    <div className="flex flex-row items-center justify-center pt-2 pr-4">
-                        <div className="flex flex-row items-start pt-7 pb-7 pl-4 pr-24 rounded-2xl bg-[#3D3D3D]">
-                            <img className="h-auto max-w-full" src="../../public/images/cat-icon-1.svg.png"
-                                 alt="image description"/>
-                            <p className=" pl-2 font-bold text-white">Romance</p>
-                        </div>
-                    </div>
-                    <div className="flex flex-row items-center justify-center pt-2 pr-4">
-                        <div className="flex flex-row items-start pt-7 pb-7 pl-4 pr-24 rounded-2xl bg-[#3D3D3D]">
-                            <img className="h-auto max-w-full" src="../../public/images/cat-icon-1.svg.png"
-                                 alt="image description"/>
-                            <p className=" pl-2 font-bold text-white">Romance</p>
+                            <p className="pl-2 font-bold text-white text-[12px]">Science Fiction & Fantasy</p>
                         </div>
                     </div>
                 </div>
-                <div className="flex flex-row items-center justify-center ">
-                    <div className="flex flex-row items-center justify-center pt-2 pr-4">
-                        <div className="flex flex-row items-start pt-7 pb-7 pl-4 pr-24 rounded-2xl bg-[#3D3D3D]">
-                            <img className="h-auto max-w-full" src="../../public/images/cat-icon-1.svg.png"
+
+                <div className="flex flex-wrap justify-center gap-6 mt-4">
+                    <div className="flex flex-col items-center justify-center pt-2">
+                        <div
+                            className="flex items-center h-full w-[250px] md:w-[220px] pl-4 py-7 pr-8 rounded-2xl bg-[#3D3D3D]">
+                            <img className="h-auto max-w-full" src="../../public/images/book.svg"
                                  alt="image description"/>
-                            <p className=" pl-2 font-bold text-white">Romance</p>
+                            <p className="pl-2 font-bold text-white text-[12px]">Literature & Fiction</p>
                         </div>
                     </div>
-                    <div className="flex flex-row items-center justify-center pt-2 pr-4">
-                        <div className="flex flex-row items-start pt-7 pb-7 pl-4 pr-24 rounded-2xl bg-[#3D3D3D]">
-                            <img className="h-auto max-w-full" src="../../public/images/cat-icon-1.svg.png"
+                    <div className="flex flex-col items-center justify-center pt-2">
+                        <div
+                            className="flex items-center h-full w-[250px] md:w-[220px] pl-4 py-7 pr-8 rounded-2xl bg-[#3D3D3D]">
+                            <img className="h-auto max-w-full" src="../../public/images/inkPot.svg"
                                  alt="image description"/>
-                            <p className=" pl-2 font-bold text-white">Romance</p>
+                            <p className="pl-2 font-bold text-white text-[12px]">Biographies & Memoirs</p>
                         </div>
                     </div>
-                    <div className="flex flex-row items-center justify-center pt-2 pr-4">
-                        <div className="flex flex-row items-start pt-7 pb-7 pl-4 pr-24 rounded-2xl bg-[#3D3D3D]">
-                            <img className="h-auto max-w-full" src="../../public/images/cat-icon-1.svg.png"
+                    <div className="flex flex-col items-center justify-center pt-2">
+                        <div
+                            className="flex items-center h-full w-[250px] md:w-[220px] pl-4 py-7 pr-8 rounded-2xl bg-[#3D3D3D]">
+                            <img className="h-auto max-w-full" src="../../public/images/heart.svg"
                                  alt="image description"/>
-                            <p className=" pl-2 font-bold text-white">Romance</p>
+                            <p className="pl-2 font-bold text-white text-[12px]">Self-Improvement</p>
                         </div>
                     </div>
-                    <div className="flex flex-row items-center justify-center pt-2 pr-4">
-                        <div className="flex flex-row items-start pt-7 pb-7 pl-4 pr-24 rounded-2xl bg-[#3D3D3D]">
-                            <img className="h-auto max-w-full" src="../../public/images/cat-icon-1.svg.png"
+                    <div className="flex flex-col items-center justify-center pt-2">
+                        <div
+                            className="flex items-center h-full w-[250px] md:w-[220px] pl-4 py-7 pr-8 rounded-2xl bg-[#3D3D3D]">
+                            <img className="h-auto max-w-full" src="../../public/images/fire.svg"
                                  alt="image description"/>
-                            <p className=" pl-2 font-bold text-white">Romance</p>
+                            <p className="pl-2 font-bold text-white text-[12px]">Erotica</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-wrap justify-center gap-6 mt-4">
+                    <div className="flex flex-col items-center justify-center pt-2">
+                        <div
+                            className="flex items-center h-full w-[250px] md:w-[220px] pl-4 py-7 pr-8 rounded-2xl bg-[#3D3D3D]">
+                            <img className="h-auto max-w-full" src="../../public/images/fire.svg"
+                                 alt="image description"/>
+                            <p className="pl-2 font-bold text-white text-[12px]">Politics & Social Sciences</p>
+                        </div>
+                    </div>
+                    <div className="flex flex-col items-center justify-center pt-2">
+                        <div
+                            className="flex items-center h-full w-[250px] md:w-[220px] pl-4 py-7 pr-8 rounded-2xl bg-[#3D3D3D]">
+                            <img className="h-auto max-w-full" src="../../public/images/innerHeart.svg"
+                                 alt="image description"/>
+                            <p className="pl-2 font-bold text-white text-[12px]">Relationships</p>
+                        </div>
+                    </div>
+                    <div className="flex flex-col items-center justify-center pt-2">
+                        <div
+                            className="flex items-center h-full w-[250px] md:w-[220px] pl-4 py-7 pr-8 rounded-2xl bg-[#3D3D3D]">
+                            <img className="h-auto max-w-full" src="../../public/images/case.svg"
+                                 alt="image description"/>
+                            <p className="pl-2 font-bold text-white text-[12px]">Startups, Business & Careers</p>
+                        </div>
+                    </div>
+                    <div className="flex flex-col items-center justify-center pt-2">
+                        <div
+                            className="flex items-center h-full w-[250px] md:w-[220px] pl-4 py-7 pr-8 rounded-2xl bg-[#3D3D3D]">
+                            <img className="h-auto max-w-full" src="../../public/images/heartCheck.svg"
+                                 alt="image description"/>
+                            <p className="pl-2 font-bold text-white text-[12px]">Health & Wellness</p>
                         </div>
                     </div>
                 </div>
             </div>
-
         </div>
-
     )
 }
 export default HomeScreen
